117415bcb6ea3cb6fee2b0a3387fa138bcaf5c46
RaDevy <ranjithrajesh256@gmail.com>
2022-03-15 20:05:17 +1000

    updated product controller logic

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -10,4 +10,8 @@ class ProductController < ApplicationController
     def complete 
         @product = Product.find(params[:id])
     end
+
+    def new
+        Product.new(title: params[:title], description:params[:description], price:params[:price], user_id:current_user.id).save!
+    end
 end
