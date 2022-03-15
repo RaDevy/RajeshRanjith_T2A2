ce706772b2582daf348621047cdef4c1b03ae4b7
RaDevy <ranjithrajesh256@gmail.com>
2022-03-15 20:06:16 +1000

    started new product logic

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get "/product", to: "product#index", as:  "products"
   get "/product/:id", to: "product#show", as:  "product"
   get "/product/:id/complete", to: "product#complete", as:  "product_complete"
+  post "/product/:new_product", to: "product#new", as:  "new_product"
   devise_for :users
 end
