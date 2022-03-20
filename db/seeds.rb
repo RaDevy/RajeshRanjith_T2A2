4efff0eefb8a4a0b3110472082c5fa373822c252
RaDevy <ranjithrajesh256@gmail.com>
2022-03-20 13:39:44 +1000

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.new(title:"Trousers", description:"Hemp based pants that are amazing", price:50, user_id:1).save!
-Product.new(title:"soap", description:"An eco-friendly alternative", price:20, user_id:1).save!
-Product.new(title:"hand_cream", description:"An eco-friendly alternative", price:35, user_id:1).save!
-Product.new(title:"tooth_paste", description:"An eco-friendly alternative", price:15, user_id:1).save!
+Product.new(title:"T_Shirts", description:"Hemp based pants that are amazing", price:50, user_id:1).save!
+Product.new(title:"Eco_Soap", description:"An eco-friendly alternative", price:20, user_id:1).save!
+Product.new(title:"Hand_cream", description:"An eco-friendly alternative", price:35, user_id:1).save!
+Product.new(title:"Tooth_Paste", description:"An eco-friendly alternative", price:15, user_id:1).save!
 Product.new(title:"Lip_Stick", description:"An eco-friendly alternative", price:25, user_id:1).save!
 Product.new(title:"Hand_bag", description:"An eco-friendly alternative", price:70, user_id:1).save!
 Product.new(title:"Scarf", description:"An eco-friendly alternative", price:40, user_id:1).save!
